21f70ddc6d444253b565eda0eaa5a4245b09e56c
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-03-17 20:06:24 +0900

    コンポーネント

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
+<script setup lang="ts">
+import TheFooter from '@/components/TheFooter.vue'
+import TheHeader from '@/components/TheHeader.vue'
+</script>
+
 <template>
   <div class="wrap">
-    <header>ヘッダー</header>
+    <TheHeader />
     <main class="main">メイン</main>
-    <footer>フッター</footer>
+    <TheFooter />
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.wrap {
+  display: flex;
+  flex-direction: column; /*上から下に*/
+  align-items: center;
+  min-height: 90vh;
+  padding: 0;
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f8f8f8;
+}
+.main {
+  flex: 1; /*親要素のwrapの高さmin-height: 90vh;とっていてmainはwrap90%全てをとっている*/
+  padding: 16px;
+}
+</style>
