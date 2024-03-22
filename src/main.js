8b30d6ade9c5a7174c54f81c31ca5d90cbb06d06
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-03-22 14:36:32 +0900

    router使用準備

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router form './router';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory} form 'vue-router';
+
+const router = createRouter({
+  history = createWebHistory(),
+  routes:[]
+});
+
+export default router;
