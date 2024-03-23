cfd4b8b2629edc752e2d2b7d4969cc5e22991ec8
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-03-23 11:13:04 +0900

    動的ルーター設定

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import TheFooter from '@/components/TheFooter.vue'
 
 const router = useRouter()
 const goBlog = () => {
-  router.push('/blog')
+  router.push({ name: 'blogId', params: { id: 1 } })
 }
 </script>
 
@@ -14,7 +14,9 @@ const goBlog = () => {
     <TheHeader />
     <nav>
       <router-link to="/">Todo</router-link>
-      | <router-link to="/about">About</router-link> | <span @click="goBlog">Blog</span>
+      | <router-link to="/about">About</router-link> | <span @click="goBlog">Blog</span> |
+      <router-link to="{ name: 'blog', params: {id: 1}}">Bolg1</router-link> |
+      <router-link to="/blog/2">Blog2</router-link>
     </nav>
     <main class="main"><router-view /></main>
     <TheFooter />
